bcf3e6136389ba18256dcf5199951688d992db43
Brandon0794 <165231072+Brandon0794@users.noreply.github.com>
2025-06-10 22:00:56 -0600

    commit del primer apiclienten
    se hizo el primer api cliente

---
+++ controller/ClienteAPICOntroller.php
@@ -0,0 +1,53 @@
+<?php
+
+require_once __DIR__.'/../accessoDatos/ClientesDAO.php';
+
+
+class ClientesApiController{
+
+
+    private $dao;
+
+    public function __construct(){
+        $this->dao = new ClientesDAO();
+    }
+
+
+    
+    public function manejarRequest(){
+        $metodo = $_SERVER['REQUEST_METHOD'];
+
+        //POST, GET, PUT DELETE
+        switch ($metodo) 
+        {
+            case 'GET':
+                # code...
+                    $response = $this->dao->obtenerDatos();
+                    echo json_encode($response);
+                break;
+
+            case 'POST':
+                # code...
+                    $datos = json_decode(file_get_contents("php://input"), true);
+
+                                    
+                    $idCliente = $datos['idCliente'];
+                    $nombre = $datos['nombre'];
+                    $correo = $datos['correo'];
+
+                    $objeto = new Clientes(null, $idCliente, $nombre, $correo);
+
+                    $this->dao->insertar($objeto);
+
+
+                    echo json_encode(["mensaje" => "Datos almacenados"]);
+
+                break;   
+        
+        }
+    }
+
+
+}
+
+?>
